25e464beb570d19ade48bbb8f1f290c7362a4400
GriffinDehne <48571799+GriffinDehne@users.noreply.github.com>
2019-06-06 12:44:28 -0700

    Add files via upload

--- code/work/work.js
+++ code/work/work.js
@@ -2,6 +2,10 @@ $(function(){
 
     $('.zineClick').click(function(){
         window.location = 'zineWork.html';
-    });  
+    }); 
+
+    $('.sovaClick').click(function(){
+        window.location = 'sovaWork.html';
+    }); 
 
 });
