face1b24e796d1fc3d0593860bc25d4dc92c7165
GriffinDehne <48571799+GriffinDehne@users.noreply.github.com>
2019-06-09 13:11:00 -0600

    Add files via upload

--- code/work/work.js
+++ code/work/work.js
@@ -1,11 +1,33 @@
 $(function(){
 
+
+    //zine click
+
     $('.zineClick').click(function(){
         window.location = 'zineWork.html';
-    }); 
+    });
+    
+    $('zineClick').touchstart(function(){
+        $(this).css('background-color', 'red');
+    });
+
+    $('zineClick').touchend(function(){
+        window.location = 'zineWork.html';
+    });
+
+//sova click
 
     $('.sovaClick').click(function(){
         window.location = 'sovaWork.html';
+    });
+
+    $('sovaClick').touchstart(function(){
+        $(this).css('background-color', 'red');
+    });
+
+    $('.sovaClick').touchend(function(){
+        window.location = 'sovaWork.html';
     }); 
+
 
 });
